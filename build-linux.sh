400397e3bdd9763e89326c135a77b3bc72a7f8bb
Michael Schoebel <mschoebel@mschoebel.de>
2014-06-25 22:46:49 +0200

    Reorganized the FPC/Linux build-script

--- build-linux.sh
+++ build-linux.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
 echo "Starting Linux build of OpenAcoon..."
 echo "------------------------------------"
-rm *.o
-rm *.ppu
+rm src/*.o
+rm src/*.ppu
+rm src/cgi/*.o
+rm src/cgi/*.ppu
+mkdir bin
+rm bin/*
 set -e
 compiler="fpc -Mdelphi -Tlinux -O3 -vew -Sew -vq"
 
 cd src
-$compiler Sleep.dpr
-$compiler CleanUrlsTxt.dpr
-$compiler ConsolidateRedirects.dpr
-$compiler ImportAlexa.dpr
-$compiler ImportData.dpr
-$compiler PrepareRobot.dpr
-$compiler Tests.dpr
+
 $compiler ImportUrls.dpr
+mv ImportUrls ../bin
+
+$compiler CleanUrlsTxt.dpr
+mv CleanUrlsTxt ../bin
+
+$compiler PrepareRobot.dpr
+mv PrepareRobot ../bin
+
+$compiler ImportData.dpr
+mv ImportData ../bin
+
+$compiler Sleep.dpr
+mv Sleep ../bin
+
 $compiler Parser.dpr
-$compiler GenDb.dpr
+mv Parser ../bin
+
+$compiler Robot.dpr
+mv Robot ../bin
+
 $compiler RobotNew.dpr
+mv RobotNew ../bin
+
+$compiler GenDb.dpr
+mv GenDb ../bin
+
 $compiler SearchServer.dpr
+mv SearchServer ../bin
+
+$compiler cgi/query.dpr
+mv /cgi/query ../bin
+
 cd ..
